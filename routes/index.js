eee82045d0ede5ed9c41bb24f754958fa67c4f88
Justin Pu <justinpu@rocketmail.com>
2017-11-15 17:57:35 -0500

    Add all pages
    Added all pages and navbar links

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,22 @@ router.get('/flock', function(req, res, next) {
     res.render('flock', { title: 'Waterloop' });
 });
 
+router.get('/media', function(req, res, next) {
+    res.render('media', { title: 'Waterloop' });
+});
+
+router.get('/sponsors', function(req, res, next) {
+    res.render('sponsors', { title: 'Waterloop' });
+});
+
+router.get('/downloads', function(req, res, next) {
+    res.render('downloads', { title: 'Waterloop' });
+});
+
+router.get('/shop', function(req, res, next) {
+    res.render('shop', { title: 'Waterloop' });
+});
+
 router.get('/contact', function(req, res, next) {
     res.render('contact', { title: 'Waterloop' });
 });
