9086cbabeb9473cd701217203bb27003416ae575
nikolaevra <Baller2000Dollar>
2017-12-01 18:19:00 -0500

    - added twitter and instagram apis

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const nodemailer = require('nodemailer');
+const Instagram = require('node-instagram').default;
 const slackmailer = require('slack-node');
+const twitter = require('twitter');
 const secret = require('../secret/index.json');
 const email = secret.email_account;
 const slack = secret.slack_account;
@@ -36,7 +38,7 @@ module.exports = {
         let slackClient = new slackmailer(slack.api_token);
         console.log(data.firstname);
 
-       slackClient.api('chat.postMessage', {
+        slackClient.api('chat.postMessage', {
             token: slack.api_token,
             text: `*Incoming contact us request:*\n*Name:* \`${data.firstname} ${data.lastname}\`\n*Email:* \`${data.email}\`\n*Subject:* ${data.subject}\n*Message:* ${data.msg}`,
             channel: '#contact_us',
@@ -46,6 +48,41 @@ module.exports = {
                 callback(false);
             } else {
                 callback(true);
+            }
+        });
+    },
+
+    getInstaPosts: function (callback) {
+        const instagram = new Instagram({
+            clientId: secret.insta_account.client_id,
+            clientSecret: secret.insta_account.client_id,
+            accessToken: secret.insta_account.access_token,
+        });
+
+        instagram.get('users/self/media/recent', (err, data) => {
+            if (err) {
+                callback(err);
+            } else {
+                callback(data);
+            }
+        });
+    },
+
+    getTweeterPosts: function (callback) {
+        const twitterClient = new twitter({
+            consumer_key: secret.twitter_account.consumer_key,
+            consumer_secret: secret.twitter_account.consumer_secret,
+            access_token_key: secret.twitter_account.access_token_key,
+            access_token_secret: secret.twitter_account.access_token_secret
+        });
+
+        let params = {screen_name: 'team_waterloop'};
+
+        twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
+            if (!error) {
+                callback(tweets);
+            } else {
+                callback(response);
             }
         });
     }
