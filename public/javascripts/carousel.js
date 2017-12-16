34b19fb7eb03f3a484fad95fd0eb57297d466323
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2017-12-16 13:38:16 -0500

    Fixed front page carousel

--- public/javascripts/carousel.js
+++ public/javascripts/carousel.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
-      $('.carousel').carousel({dist: -50, padding: 100, duration: 500, fullWidth: true});
-      $('.media-carousel').carousel({height: "20vh", width: 500 , duration: 500, fullWidth: true});
+      $('.carousel').carousel({dist: -50, padding: 100, duration: 500});
     });
 
 setInterval(function(){
   $('.carousel').carousel('next');
-  $('.media-carousel').carousel('next');
-
 }, 5000);
---
+++ public/javascripts/mediaCarousel.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+      $('.media-carousel').carousel({height: "20vh", width: 500 , duration: 500, fullWidth: true});
+    });
+
+setInterval(function(){
+  $('.media-carousel').carousel('next');
+
+}, 5000);
