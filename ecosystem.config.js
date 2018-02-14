ff39e0c036a45df160a4ee668860aedfa2a88b12
EC2 Default User <ec2-user@ip-172-31-82-36.ec2.internal>
2018-02-14 23:53:17 +0000

    oh man

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
     // First application
     {
-      name      : 'prod-web',
+      name      : 'web',
       script    : 'bin/www',
       env: {
         COMMON_VARIABLE: 'true'
@@ -16,18 +16,8 @@ module.exports = {
         NODE_ENV: 'production'
       }
     },
-
-    // Second application
-    // {
-    //  name      : 'WEB',
-    //  script    : 'web.js'
-    //}
   ],
 
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
   deploy : {
     production : {
       user : 'ec2-user',
@@ -36,23 +26,12 @@ module.exports = {
       key  : '~/.ssh/instance-key.pem',
       repo : 'git@github.com:waterloop/waterloop-website.git',
       path : '/home/ec2-user/prod/waterloop-website',
-      "post-setup": "cp -R ~/cred/secret/ ~/prod/waterloop-website/source/;ls ~/prod/waterloop-website/source/",
+      "post-setup": "ls ~/cred;pwd;cp -R ~/cred/secret/ ~/prod/waterloop-website/source/;ls ~/prod/waterloop-website/source/",
       "pre-deploy-local" : "pwd;ls -la",
       'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js production',
       "env"  : {
         "NODE_ENV": "production"
       }
-   }
-//    dev : {
-//      user : 'node',
-//      host : '212.83.163.1',
-//      ref  : 'origin/master',
-//      repo : 'git@github.com:repo.git',
-//      path : '/var/www/development',
-//      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-//      env  : {
-//        NODE_ENV: 'dev'
-//      }
-//    }
+    }
   }
 };
